8b8849d35d006d4f9795d45a621f8c5d4c244395
Manzi Fabrice <manzif60@gmail.com>
2021-01-21 15:12:28 +0200

    add MongoDb to nest api products

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-
+import { MongooseModule } from '@nestjs/mongoose'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, MongooseModule.forRoot('mongodb+srv://admin:s1vpQrTWSRXMde6V@cluster0.96yte.mongodb.net/nestjs-products?retryWrites=true&w=majority')],
   controllers: [AppController],
   providers: [AppService],
 })
